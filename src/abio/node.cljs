6ad32a9360b90629ab34e7601e070b6743bfacc0
Mike Fikes <mike@fikesfarm.com>
2017-01-05 14:09:40 -0500

    Intial attempt

---
+++ src/abio/node.cljs
@@ -0,0 +1,14 @@
+(ns abio.node
+  (:require
+    [abio.io]))
+
+(defrecord IOOps [fs]
+  abio.io/IIOOps
+  (-directory? [this f]
+    (.. fs (lstatSync f) (isDirectory)))
+  (-list-files [this d])
+  (-delete-file [this f]))
+
+(defn io-ops
+  []
+  (->IOOps (js/require "fs")))
