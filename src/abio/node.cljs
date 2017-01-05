3f1753e9ed33fdacdb5448af0c2309ca79d51bd6
Mike Fikes <mike@fikesfarm.com>
2017-01-05 18:14:06 -0500

    Support for reading

--- src/abio/node.cljs
+++ src/abio/node.cljs
@@ -1,13 +1,22 @@
 (ns abio.node
   (:require
-    [abio.io]))
+    [abio.io]
+    [clojure.string :as string]))
 
 (defrecord IOOps [fs]
   abio.io/IIOOps
   (-directory? [this f]
     (.. fs (lstatSync f) (isDirectory)))
   (-list-files [this d])
-  (-delete-file [this f]))
+  (-delete-file [this f])
+  (-file-reader-open [this path encoding]
+    (let [fd (.openSync fs path "r")
+          read-stream (.createReadStream fs nil #js {:fd fd :encoding encoding})]
+      (.pause read-stream)
+      (.read read-stream)                                   ; Hack to get buffer primed
+      (abio.io/->BufferedReader #(.read read-stream) #(.close fs fd) (atom nil) (atom 0))))
+  (-file-reader-read [this reader])
+  (-file-reader-close [this reader]))
 
 (defn bindings
   []
