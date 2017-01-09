ee7173f69bd4e61fabaa217ec490d807c20de1cb
Mike Fikes <mike@fikesfarm.com>
2017-01-08 21:43:46 -0500

    Update to follow abio

--- src/abio/node.cljs
+++ src/abio/node.cljs
@@ -1,59 +1,8 @@
 (ns abio.node
   (:require
-    [abio.io]
-    [clojure.string :as string]))
-
-(defrecord BufferedReader [raw-read raw-close buffer pos]
-  abio.io/IReader
-  (-read [_]
-    (if-some [buffered @buffer]
-      (do
-        (reset! buffer nil)
-        (subs buffered @pos))
-      (raw-read)))
-  abio.io/IBufferedReader
-  (-read-line [this]
-    (if-some [buffered @buffer]
-      (if-some [n (string/index-of buffered "\n" @pos)]
-        (let [rv (subs buffered @pos n)]
-          (reset! pos (inc n))
-          rv)
-        (if-some [new-chars (raw-read)]
-          (do
-            (reset! buffer (str (subs buffered @pos) new-chars))
-            (reset! pos 0)
-            (recur this))
-          (do
-            (reset! buffer nil)
-            (let [rv (subs buffered @pos)]
-              (if (= rv "")
-                nil
-                rv)))))
-      (if-some [new-chars (raw-read)]
-        (do
-          (reset! buffer new-chars)
-          (reset! pos 0)
-          (recur this))
-        nil)))
-  abio.io/IClosable
-  (-close [_]
-    (raw-close)))
-
-(defrecord IOOps [fs]
-  abio.io/IIOOps
-  (-directory? [this f]
-    (.. fs (lstatSync f) (isDirectory)))
-  (-list-files [this d])
-  (-delete-file [this f])
-  (-file-reader-open [this path encoding]
-    (let [fd (.openSync fs path "r")
-          read-stream (.createReadStream fs nil #js {:fd fd :encoding encoding})]
-      (.pause read-stream)
-      (.read read-stream)                                   ; Hack to get buffer primed
-      (->BufferedReader #(.read read-stream) #(.close fs fd) (atom nil) (atom 0))))
-  (-file-reader-read [this reader])
-  (-file-reader-close [this reader]))
+    [abio.node.io :as io]))
 
 (defn bindings
   []
-  {:abio/io-ops (->IOOps (js/require "fs"))})
+  (merge
+    (io/bindings)))
---
+++ src/abio/node/io.cljs
@@ -0,0 +1,59 @@
+(ns abio.node.io
+  (:require
+    [abio.io :as io]
+    [clojure.string :as string]))
+
+(defrecord BufferedReader [raw-read raw-close buffer pos]
+  abio.io/IReader
+  (-read [_]
+    (if-some [buffered @buffer]
+      (do
+        (reset! buffer nil)
+        (subs buffered @pos))
+      (raw-read)))
+  abio.io/IBufferedReader
+  (-read-line [this]
+    (if-some [buffered @buffer]
+      (if-some [n (string/index-of buffered "\n" @pos)]
+        (let [rv (subs buffered @pos n)]
+          (reset! pos (inc n))
+          rv)
+        (if-some [new-chars (raw-read)]
+          (do
+            (reset! buffer (str (subs buffered @pos) new-chars))
+            (reset! pos 0)
+            (recur this))
+          (do
+            (reset! buffer nil)
+            (let [rv (subs buffered @pos)]
+              (if (= rv "")
+                nil
+                rv)))))
+      (if-some [new-chars (raw-read)]
+        (do
+          (reset! buffer new-chars)
+          (reset! pos 0)
+          (recur this))
+        nil)))
+  abio.io/IClosable
+  (-close [_]
+    (raw-close)))
+
+(defrecord Bindings [fs]
+  abio.io/IBindings
+  (-directory? [this f]
+    (.. fs (lstatSync f) (isDirectory)))
+  (-list-files [this d])
+  (-delete-file [this f])
+  (-file-reader-open [this path encoding]
+    (let [fd (.openSync fs path "r")
+          read-stream (.createReadStream fs nil #js {:fd fd :encoding encoding})]
+      (.pause read-stream)
+      (.read read-stream)                                   ; Hack to get buffer primed
+      (->BufferedReader #(.read read-stream) #(.close fs fd) (atom nil) (atom 0))))
+  (-file-reader-read [this reader])
+  (-file-reader-close [this reader]))
+
+(defn bindings
+  []
+  {::io/bindings (->Bindings (js/require "fs"))})
