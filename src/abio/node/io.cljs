3fd94b2fb707afd663ec630749838266da70043b
Evan Niessen-Derry <eniessenderry@gmail.com>
2018-05-11 23:00:25 -0500

    Unify async/sync reader/writer protocols
    It makes more sense to me to have a single protocol method for a
    concept that has two different arities to handle sync/async
    functionality. This simplifies the amount of code necessary, as well as
    making it easier to switch between sync/async (just drop/add an
    argument).
    
    As such, we don't have async file readers/writers anymore. We just have
    `file-reader-open` and `file-writer-open`, which return reader/writer
    records. These records then have methods that can be called to
    read/write, and depending on the arity you use you get synchronicity or
    asynchronicity.
    
    As such, if you want to do a sync and async read, it would be something
    like
    
    ```
    ;; Reify your reader record
    (def rdr (abio.io/reader "project.clj" :encoding "utf8"))
    ;; Sync
    (print (abio.io/-read rdr))
    ;; Async
    (abio.io/-read rdr (fn [err data] (println)(print data)))
    ```

--- src/abio/node/io.cljs
+++ src/abio/node/io.cljs
@@ -10,47 +10,29 @@
   abio.io/IReader
   (-read [_]
     (.readFileSync fs path (clj->js opts)))
-  (-read [_ _] (throw (ex-info "No double arity -read for a synchronous Node BufferedReader" {})))
+  ;; TODO: `cb` has to be 2-arity, first param is errors and second is data
+  (-read [_ cb]
+    ;; TODO if this is a fd it won't get closed automatically; eventually that needs to be checked for.
+    (.readFile fs path (clj->js opts) cb))
 
   abio.io/IClosable
   ;; Even though there's nothing to close here, we keep it to preserve the use of `with-open`
   (-close [_] nil))
 
-;; TODO I could also potentially just make the 2-arity function in BufferedReader async
-;;      This might simplify the code some
-(defrecord AsyncBufferedReader [path opts fs] ; `opts` needs :encoding and optionally :flag TODO how do I want to enforce that?
-  abio.io/IReader
-  (-read [_] (throw (ex-info "No single arity -read for AsyncBufferedReader" {})))
-  (-read [_ cb] ; `cb` has to be 2-arity, first param is errors and second is data
-    (.readFile fs path (clj->js opts) cb)) ; TODO if this is a fd it won't get closed automatically; eventually that needs to be checked for.
-
-  abio.io/IClosable
-  (-close [_]
-    ;; TODO: should these `-close` functions return true or nil?
-    true))
-
 (defrecord BufferedWriter [path opts fs]
   abio.io/IAbioWriter
   (-write [_ output]
     (.writeFileSync fs path output (clj->js opts)))
-  (-write [_ output _]
-    (throw (ex-info "No double arity -write for BufferedWriter" {})))
+  ;; XXX `cb` takes one arg, `err`, but otherwise execution signals completion of the write.`
+  ;; TODO: is there a good way to signal that, or do we have to just assume the
+  ;; consumer of the node library know it?
+  (-write [_ output cb]
+    (.writeFile fs path output (clj->js opts) cb))
 
   abio.io/IClosable
   (-close [_]
     nil))
 
-(defrecord AsyncBufferedWriter [path opts fs] ; TODO can the `flags` actually be kept as a map `opts`?
-  abio.io/IAbioWriter
-  (-write [_ output]
-    (throw (ex-info "No single arity -write for AsyncBufferedWriter" {})))
-  (-write [_ output cb] ; XXX `cb` takes one arg, `err`, but otherwise execution signals completion of the write.`
-    (.writeFile fs path output (clj->js opts) cb))
-
-  abio.io/IClosable
-  (-close [_] true))
-
-;; TODO: add some js->clj to this? More broadly, how/should we offer up cljs data?
 (defrecord Bindings [fs sep]
   abio.io/IBindings
   (-path-sep [this] sep)
@@ -62,17 +44,12 @@
     (.. fs (readdir d cb)))
   (-delete-file [this f])
 
-
   (-file-reader-open [this path opts]
     (->BufferedReader path opts fs))
-  (-async-file-reader-open [this path opts]
-    (->AsyncBufferedReader path opts fs))
 
   ;; Default to non-destructive write
   (-file-writer-open [this path opts]
-    (->BufferedWriter path (merge {:flag "a"} opts) fs))
-  (-async-file-writer-open [this path opts]
-    (->AsyncBufferedWriter path (merge {:flag "a"} opts) fs)))
+    (->BufferedWriter path (merge {:flag "a"} opts) fs)))
 
 (defn bindings
   []
